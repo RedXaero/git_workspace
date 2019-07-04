20f159a71d618b159167a29d5ab52153b755c0f6
RedXaero <FarithEMartinez@hotmail.com>
2019-07-04 13:15:27 -0500

    11150 fue aceptado

--- UVa/C/11150_-_Cola.c
+++ UVa/C/11150_-_Cola.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
-int suma(int, int);
+/**
+Accepted
+*/
 
+void div(int *, int*, int*);
 int main()
 {
 
-	int N, aux;
+	int N, aux, suma = 0;
+	
 
-	while(scanf("%d", &N) != EOF)
+	while( (scanf("%d", &N)) != EOF)
 	{
-		if(N % 2)
+		if(N == 1)
+			suma = 1;
+
+		else if(N % 2)
 		{
-			N++;
-
-			while(N / 3){
-				aux = N / 3;
-				N %= 3;
-
-				
-			}
-
+			div(&N, &aux, &suma);
+			suma +=  N;
+			suma--;
 		}
+		else
+		{
+			++N;
+			div(&N, &aux, &suma);
+			--suma;
+		}
+		
+		printf("%d\n", suma);
+		suma = 0;
 	}
 	return 0;
+}
+
+void div(int *N, int *aux, int *suma){
+
+	while( (*N)!= 1 || (*aux) != 0)
+			{
+				(*aux) = (*N) % 3;
+				(*N) = floor((*N) /3);
+				(*suma)+=((*N)*3);
+				(*N) += (*aux);
+			}
+			(*suma) +=  (*N);
 }
