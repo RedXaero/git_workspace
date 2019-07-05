eef51d4f51ae6ee63ee526040a0fac88a85a958c
RedXaero <50529709+RedXaero@users.noreply.github.com>
2019-07-04 19:00:30 -0500

    Update 100-The_3n_+_1_problem.c

--- UVa/C/100-The_3n_+_1_problem.c
+++ UVa/C/100-The_3n_+_1_problem.c
@@ -8,7 +8,6 @@ int main(){
 	unsigned int a, b, cont, max = 0, c;
 	unsigned int a1, b1, i, n;
 	
-
 	while(scanf("%d %d", &a, &b) != EOF){
 		a1 = a;
 		b1 = b;
@@ -28,27 +27,19 @@ int main(){
 
 				if( (n%2) == 1)
 					n = 3 * n + 1;
-
 				else
 					n /= 2;
-
 				cont++;
 			}
 
 			if(cont > max)
 				max = cont;
-
 			cont = 0;
 		}
 
 		printf("%d %d %d\n", a1, b1,++max);
-
 		max = 0;
 	}
-
-	
-
-
 
 	return 0;
 }
