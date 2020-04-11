8be042fbda7716c57dab18f1e327106f734ec326
RedXaero <50529709+redxaero@users.noreply.github.com>
2020-04-11 00:23:26 -0500

    Primera aproximación 492-Pig-Latin.c; en construcción...

--- UVa/C/10878_-_Decode_the_tape.c
+++ UVa/C/10878_-_Decode_the_tape.c
@@ -13,44 +13,27 @@ int main(){
 	gets(vec);
 
 	while(gets(vec)){
-		
 		if(vec[1] == 'o')
 			i+= 128;
-
 		if(vec[2] == 'o')
 			i+= 64;
-
 		if(vec[3] == 'o')
 			i+= 32;
-
 		if(vec[4] == 'o')
 			i+= 16;
-
 		if(vec[5] == 'o')
 			i+= 8;
-
 		if(vec[7] == 'o')
 			i+= 4;
-
 		if(vec[8] == 'o')
 			i+= 2;
-
 		if(vec[9] == 'o')
 			i+= 1;
-
 		if(i == 10)
 			printf("\n");
 		else
 			printf("%c", i);
 		i = 0;
-
-
 	}
-
-	
-
-	
-
 	return  0;
-
 }
--- UVa/C/492-Pig-Latin.c
+++ UVa/C/492-Pig-Latin.c
@@ -3,10 +3,28 @@
 //
 
 #include <stdio.h>
+void separa(char [], int, int);
 
 int main(){
 
-    printf("Pig latin");
+    int i,j;
+    char cad[2000];
 
+    i = j = 0;
+    while(fgets(cad, 2000, stdin) != NULL)
+    {
+        while( (cad[j] =! ' ') && (cad[j] != '\0'))
+            ++j;
+        separa(cad, i,j);
+    }
     return 0;
+}
+
+void separa(char *cad, int i, int j){
+    int cont;
+    char aux;
+    aux = &cad[i];
+
+
+
 }
