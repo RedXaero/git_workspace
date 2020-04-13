f0e8e1152817a04f7e04e83cc11196f4386d8068
RedXaero <farithemartinez@hotmail.com>
2020-04-12 21:01:20 -0500

    Se agregó gitignore

--- UVa/C/492-Pig-Latin.c
+++ UVa/C/492-Pig-Latin.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int i,j;
+    int i;
     char cad[2000], aux;
     i = 0;
-    while(fgets(cad, 2000, stdin)){
-        while(cad[i] != '\0' && cad[i] != '\n'){
+    while(fgets(cad, 5000, stdin)){
+        while(cad[i] != '\0'){
             if((cad[i] >= 'a' && cad[i] <= 'z') || (cad[i] >= 'A' && cad[i] <= 'Z')){
                 if(cad[i] == 'a' || cad[i] == 'e' || cad[i] == 'i' || cad[i] == 'o' || cad[i] == 'u'){
                     while((cad[i] >= 'a' && cad[i] <= 'z') || (cad[i] >= 'A' && cad[i] <= 'Z')){
@@ -38,44 +38,7 @@ int main(){
             }
             ++i;
         }
-        if(cad[i] == '\n')
-            printf("\n");
         i = 0;
     }
     return 0;
 }
-
-/*
-#include<stdio.h>
-#include<ctype.h>
-int main()
-{
-    char ch,save;
-    int n;
-    while(scanf("%c",&ch)==1)
-    {
-        if((ch=='a'|| ch=='A'||ch=='e'|| ch=='E'||ch=='i'|| ch=='I'||ch=='u'|| ch=='U'||ch=='o'|| ch=='O'))
-        {
-            printf("%c",ch);
-            while(scanf("%c",&ch)==1){
-                if(isalpha(ch)==0)
-                    break;
-                printf("%c",ch);
-            }            printf("ay%c",ch);
-        }
-        else if(isalpha(ch))
-        {
-            save=ch;
-            while(scanf("%c",&ch))
-            {
-                if(isalpha(ch)==0)
-                    break;
-                printf("%c",ch);
-            }
-            printf("%cay%c",save,ch);
-        }
-        else
-            printf("%c",ch);
-    }
-    return 0;
-}*/
