e7cb47466439db687169872ca1dd6a3b4bacfb30
Eliezer Martinez <femartinez@minsait.com>
2021-07-03 21:22:10 -0500

    Fix: Implementamos fgets, es mas seguro que gets

--- UVa/C/494-Kindergarten_Counting_Game.c
+++ UVa/C/494-Kindergarten_Counting_Game.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
 int main(){
+
+    char string[1001];
+    int i, contador = 0;
+
+    while(fgets(string, 1001, stdin)){
+        i = 0;
+        contador = 0;
+        for(int j = 0; string[j] != '\0'; j++){
+            if(string[j] != '\n'){
+                if(string[j] != ' '){/*
+                    if( (string[j]>= 'a' && string[j]<= 'z') || (string[j]>= 'A' && string[j]<= 'Z')){
+                        contador++;
+                    }*/
+                    printf("%c", string[j]);
+                }
+                else{
+                    printf("\n");
+                }
+            }
+        }
+        printf("%d", contador);
+        contador = 0;
+            
+    }
+
     return 0;
 }
+
